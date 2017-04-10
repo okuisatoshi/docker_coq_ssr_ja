fb98c7ae6d7d83f4c44d27dd82c98acf14242928
Satoshi Okui <okuisatoshi@mac.com>
2017-04-10 22:32:20 +0900

    最初のコミット

---
+++ init.el
@@ -0,0 +1,24 @@
+(require 'mozc)
+(set-language-environment "Japanese")
+(setq default-input-method "japanese-mozc")
+(prefer-coding-system 'utf-8)
+
+(defadvice coq-mode-config
+  (after deactivate-holes-mode () activate)
+  (progn (holes-mode 0))
+)
+
+(add-hook 'proof-mode-hook
+	  '(lambda ()
+	     (define-key proof-mode-map (kbd "C-c C-j") 'proof-goto-point)
+	     )
+	  )
+
+;;(setq coq-load-path-include-current t)
+(setq proof-splash-enable nil)
+;;(setq proof-three-window-mode-policy 'hybrid)
+;;(setq coq-compile-before-require t)
+
+(setq coq-prog-args
+      (cons "-R" (cons "/home/opam/.opam/4.04.0/lib/coq/user-contrib/mathcomp"
+		       (cons "mathcomp" (cons "-emacs" nil)))))
